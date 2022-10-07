e5fb3347ef77a539dcb5fc93c964f18fb5f7d7eb
Leonid Gadetsky <leonidgadetsky@yandex.ru>
2022-10-07 12:00:01 +0300

    add mybatis module

--- src/MyBatisModule.java
+++ src/MyBatisModule.java
@@ -1,4 +1,11 @@
+import org.mybatis.guice.XMLMyBatisModule;
 
-public class MyBatisModule implements XMLM{
+public class MyBatisModule extends XMLMyBatisModule{
+
+    @Override
+    protected void initialize() {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
